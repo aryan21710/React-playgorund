f66a2333daa8e27c37289cd714e94917f07e24dd
aryan21710 <aryan21710@gmail.com>
2020-09-14 13:32:28 +0530

    COUNT INCR APP WORKING

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Parent} from './components/hooksTesting/parent';
+import {Parent} from './components/hooksTesting/Parent';
 
 ReactDOM.render(
   <React.StrictMode>
