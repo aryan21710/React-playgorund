54a01d689ff8ff8d114a423fbf6d4d7a9fa65c3c
aryan21710 <aryan21710@gmail.com>
2020-10-08 18:22:39 +0530

    DURATION BASED ANIMATED SLIDER WORKING WITH REACT-SPRING

--- src/components/ReactSpringAnimation/AnimatedSlider.js
+++ src/components/ReactSpringAnimation/AnimatedSlider.js
@@ -1,37 +1,56 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { animated, useSpring } from 'react-spring';
 
 export const AnimatedSlider = () => {
-    const [isClicked, setIsClicked] = useState(false);
-    const [loadingPercentage, setLoadingPercentage] = useState(0);
+	const [isClicked, setIsClicked] = useState(false);
+	const props = useSpring({
+		width: isClicked ? 100 : 0,
+		backgroundColor: isClicked ? 'red' : 'grey',
+		config: {
+			duration: 2000,
+			delay: 1000,
+		},
+	});
 
-    useEffect(()=>{
-        isClicked ? setLoadingPercentage(100) :  setLoadingPercentage(0)
-    },[isClicked])
-
-	const MainWrapper = styled.div`
+	const MainWrapper = styled(animated.div)`
 		width: 50vw;
 		height: 50vh;
 		background: grey;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		position: relative;
+		top: 0vh;
+		left: 0vw;
 	`;
-	const Slider = styled.div`
-		width: 30vw;
-		height: 5vh;
-        border: 2px solid white;
-        display: flex;
+	const SliderWrapper = styled(animated.div)`
+		width: 98px;
+		height: 30px;
+		border: 2px solid white;
+		display: flex;
 		align-items: center;
-        justify-content: center;
-        color: white
+		justify-content: center;
+		color: white;
+		position: absolute;
+		top: 20vh;
+		left: 10vw;
+		z-index: 1;
+	`;
+
+	const AnimSlider = styled(animated.div)`
+		height: 34px;
+		position: absolute;
+		top: 20vh;
+		left: 10vw;
+		z-index: 0;
 	`;
 	return (
 		<MainWrapper>
-			<Slider
-				style={isClicked ? { background: 'red' } : { background: '' }}
-				onClick={() => setIsClicked(!isClicked)}
-			>{loadingPercentage}</Slider>
+			<SliderWrapper onClick={() => setIsClicked(!isClicked)}>
+				{props.width.interpolate((x) => Math.floor(x))}
+			</SliderWrapper>
+			<AnimSlider style={props}></AnimSlider>
 		</MainWrapper>
 	);
 };
